d569416b2baaea692f19f228b32d99dc5677e410
Panos Kyriakis <kyriakis@nevron.ai>
2023-09-29 05:37:22 +0000

    updates in testing

--- test/message_helper_test.py
+++ test/message_helper_test.py
@@ -1,4 +1,4 @@
-import os
+import unittest
 import numpy as np
 
 from metisfl.learner.message import MessageHelper
@@ -6,7 +6,7 @@ from metisfl.encryption.homomorphic import HomomorphicEncryption
 from metisfl.helpers.ckks import generate_keys
 
 
-class TestMessageHelper:
+class TestMessageHelper(unittest.TestCase):
 
     def setUp(self) -> None:
         super().setUp()
@@ -18,21 +18,22 @@ class TestMessageHelper:
         generate_keys(batch_size, scaling_factor_bits, cc, pk, prk)
         
         self.scheme = HomomorphicEncryption(batch_size, scaling_factor_bits, cc, pk, prk)
-        self.test = np.random.rand(2, 2)
 
-    def test_no_encryption(self):
+    def test_no_encryption_1(self):
+        weights = np.random.rand(2,2)
         helper = MessageHelper()
-        model = helper.weights_to_model_proto([self.test])
+        model = helper.weights_to_model_proto([weights])
         weights = helper.model_proto_to_weights(model)
         assert np.allclose(self.test, weights[0])
 
-    def test_encryption(self):
+    def test_encryption_1(self):
+        weights = np.random.rand(2,2)
         helper = MessageHelper(self.scheme)
-        model = helper.weights_to_model_proto([self.test])
+        model = helper.weights_to_model_proto([weights])
         weights = helper.model_proto_to_weights(model)
         assert np.allclose(self.test, weights[0])
 
-    def test_no_encryption_multiple(self):
+    def test_no_encryption_2(self):
         weights = np.random.rand(10,10,10,10)
         helper = MessageHelper()
         model = helper.weights_to_model_proto(weights)
@@ -40,7 +41,7 @@ class TestMessageHelper:
         for w1, w2 in zip(weights, weights_out):
             assert np.allclose(w1, w2)
 
-    def test_encryption_multiple(self):
+    def test_encryption_2(self):
         weights = np.random.rand(10,10,10,10)
         helper = MessageHelper(self.scheme)
         model = helper.weights_to_model_proto(weights)
