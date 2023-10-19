c3f6a90db31422c61735597d0b054312e9859660
Panos Kyriakis <kyriakis@nevron.ai>
2023-10-19 06:00:31 +0000

    replaced unittest with pytest

--- test/hash_map_test.py
+++ test/hash_map_test.py
@@ -1,52 +1,38 @@
-
-import unittest
-
 from metisfl.controller.store.hash_map import HashMapModelStore
 from metisfl.proto import model_pb2
 
 
-class TestHashMapModelStore(unittest.TestCase):
+def test_insert():
+    pairs = [
+        ("l1", model_pb2.Model()),
+        ("l2", model_pb2.Model()),
+    ]
+    store = HashMapModelStore(0)
+    store.insert(pairs)
+    assert len(store.store_cache) == 2
+    
+def test_select():
+    pairs = (
+        ("l1", "model_pb2.Model()"),
+        ("l2", "model_pb2.Model()"),
+        ("l1", "model_pb2.Model()"),
+        ("l2", "model_pb2.Model()"),
+    )
+    store = HashMapModelStore(0)
+    store.insert(pairs)
+    selected = store.select([("l1", 2)])
 
-    def setUp(self) -> None:
-        return super().setUp()
+    assert len(selected["l1"]) == 2
 
-    def tearDown(self) -> None:
-        return super().tearDown()
+def test_capacity():
+    pairs = (
+        ("l1", "model_pb2.Model()"),
+        ("l1", "model_pb2.Model()"),
+        ("l1", "model_pb2.Model()"),
+        ("l1", "model_pb2.Model()"),
+    )
 
-    def test_insert(self):
-        pairs = [
-            ("l1", model_pb2.Model()),
-            ("l2", model_pb2.Model()),
-        ]
-        store = HashMapModelStore(0)
-        store.insert(pairs)
-        self.assertEqual(len(store.store_cache), 2)
+    store = HashMapModelStore(2)
+    store.insert(pairs)
+    assert len(store.store_cache) == 2
 
-    def test_select(self):
-        pairs = (
-            ("l1", "model_pb2.Model()"),
-            ("l2", "model_pb2.Model()"),
-            ("l1", "model_pb2.Model()"),
-            ("l2", "model_pb2.Model()"),
-        )
-        store = HashMapModelStore(0)
-        store.insert(pairs)
-        selected = store.select([["l1", 2]])
-
-        self.assertEqual(len(selected["l1"]), 2)
-
-    def test_capacity(self):
-        pairs = (
-            ("l1", "model_pb2.Model()"),
-            ("l1", "model_pb2.Model()"),
-            ("l1", "model_pb2.Model()"),
-            ("l1", "model_pb2.Model()"),
-        )
-
-        store = HashMapModelStore(2)
-        store.insert(pairs)
-        self.assertEqual(len(store.store_cache["l1"]), 2)
-
-
-if __name__ == '__main__':
-    unittest.main()
